6d430b79f4658bc0ae0d99997cde30edcc0635e5
paulovictorln <paulovictor494@gmail.com>
2018-02-08 15:08:12 -0300

    ajustes

--- src/main/java/br/com/mv/managedbean/CadastroMB.java
+++ src/main/java/br/com/mv/managedbean/CadastroMB.java
@@ -154,10 +154,6 @@ public class CadastroMB implements Serializable {
 				new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, mensagem));
 	}
 
-	public String cadastrar() {
-
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-		return "/cadastro?faces-redirect=true";
-	}
+	
 
 }
