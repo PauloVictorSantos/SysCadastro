106e28da9fb4edcdde43ce1b280f5094f3fe118b
paulovictorln <paulovictor494@gmail.com>
2018-07-14 20:27:51 -0300

    icones na tela de atualizar

--- src/main/java/br/com/mv/utils/HibernateUtil.java
+++ src/main/java/br/com/mv/utils/HibernateUtil.java
@@ -7,11 +7,8 @@ private static final SessionFactory sessionFactory;
     
     static {
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
             sessionFactory = new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
-            // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
