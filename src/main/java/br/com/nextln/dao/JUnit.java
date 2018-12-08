83826b84009fc00afc968f2a7b243f4eb587a42a
paulovictorln <paulovictor494@gmail.com>
2018-12-08 00:22:49 -0300

    Script que baixa arquivos funcionais

--- src/main/java/br/com/nextln/dao/JUnit.java
+++ src/main/java/br/com/nextln/dao/JUnit.java
@@ -1,10 +1,18 @@
 package br.com.nextln.dao;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
@@ -18,17 +26,40 @@ public class JUnit {
         
 		webClient.getOptions().setThrowExceptionOnScriptError(false);
 		webClient.getOptions().setThrowExceptionOnScriptError(false);
-		String url = "https://facebook.com/";
-         
-        HtmlPage page = webClient.getPage(url);
-        HtmlAnchor anchor =  (HtmlAnchor) page.getByXPath("//table/tbody/tr[3]/td[2]/div/a").get(0);
-        anchor.click();
-        page.getEnclosingWindow().getEnclosedPage();
-        WebResponse response = page.getWebResponse();
-        String content = response.getContentAsString();
-        String pageContent =page.asText();
-         System.out.println(pageContent);
-        System.out.println(content);
+		
+		HtmlPage page = webClient.getPage("http://dv.parliament.bg/DVWeb/broeveList.faces");
+
+	    for (HtmlAnchor link : (List<HtmlAnchor>) page.getByXPath("//table[@id='broi_form:dataTable1']//a/img/.."))
+	    {
+	        String commandString = link.getOnClickAttribute().replaceAll("return ", "");
+	        System.out.println(commandString);
+
+	        ScriptResult executeJavaScript = page.executeJavaScript(commandString);
+
+	        Page newPage = executeJavaScript.getNewPage();
+	        System.out.println(newPage.getWebResponse().getContentAsStream());
+	        
+	        InputStream is = newPage.getWebResponse().getContentAsStream();
+            FileOutputStream output = new FileOutputStream("C:/Download/"+new Date().getTime()+".pdf");
+	        
+            IOUtils.copy(is, output);
+            output.close();
+
+            System.out.println("New file created!");     
+	        page = webClient.getPage("http://dv.parliament.bg/DVWeb/broeveList.faces");
+	    }
+
 	}
+         
+//        HtmlPage page = webClient.getPage(url);
+//        HtmlAnchor anchor =  (HtmlAnchor) page.getByXPath("//table/tbody/tr[3]/td[2]/div/a").get(0);
+//        anchor.click();
+//        page.getEnclosingWindow().getEnclosedPage();
+//        WebResponse response = page.getWebResponse();
+//        String content = response.getContentAsString();
+//        String pageContent =page.asText();
+//         System.out.println(pageContent);
+//        System.out.println(content);
+	
 
 }
